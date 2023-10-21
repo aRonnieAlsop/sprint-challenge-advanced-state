eb3c0bd949f2a4821177e364da8e2824c45feb93
Arron Alsop <alsop.arron@gmail.com>
2023-10-20 21:37:48 -0700

    'wip'

--- frontend/components/Wheel.js
+++ frontend/components/Wheel.js
@@ -39,21 +39,6 @@ function Wheel(props) {
       </button>
     </div>
   </div>
-
-    // <div id="wrapper">
-    //   <div id="wheel">
-    //     <div className="cog active" style={{ "--i": 0 }}>B</div>
-    //     <div className="cog" style={{ "--i": 1 }}></div>
-    //     <div className="cog" style={{ "--i": 2 }}></div>
-    //     <div className="cog" style={{ "--i": 3 }}></div>
-    //     <div className="cog" style={{ "--i": 4 }}></div>
-    //     <div className="cog" style={{ "--i": 5 }}></div>{/* --i is a custom CSS property, no need to touch that nor the style object */}
-    //   </div>
-    //   <div id="keypad">
-    //     <button id="counterClockwiseBtn" onClick={handleCounterClockwiseClick}>Counter clockwise</button>
-    //     <button id="clockwiseBtn" onClick={handleClockwiseClick}>Clockwise</button>
-    //   </div>
-    // </div>
   )
 }
 
