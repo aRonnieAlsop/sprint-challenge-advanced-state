28c96c0bb746ddb96ae854d77f87310c142afc31
Arron Alsop <alsop.arron@gmail.com>
2023-10-22 12:25:55 -0700

    'backtack to just Wheel working'

--- frontend/state/action-creators.js
+++ frontend/state/action-creators.js
@@ -3,9 +3,7 @@
 import { MOVE_CLOCKWISE, 
   MOVE_COUNTERCLOCKWISE,
   SET_ACTIVE_COG_INDEX,
-  SET_QUIZ_INTO_STATE,
-  SET_INFO_MESSAGE,
-  SET_SELECTED_ANSWER, } from './action-types'
+  } from './action-types'
 
 export function moveClockwise() { 
   return {
@@ -26,20 +24,13 @@ export function setActiveCogIndex(index) {
   }
 }
 
-export function setSelectedAnswer(answerIndex) {
-  return {
-    type: SET_SELECTED_ANSWER,
-    payload: answerIndex,
-  }
+export function setSelectedAnswer() {
 }
 
 export function selectAnswer() { }
 
-export function setInfoMessage(message) { 
-  return {
-    type: SET_INFO_MESSAGE,
-    payload: message,
-  }
+export function setInfoMessage() { 
+ 
 }
 
 export function setQuiz() { }
@@ -50,50 +41,15 @@ export function resetForm() { }
 
 // ❗ Async action creators
 export function fetchQuiz() {
-  return function (dispatch) {
-    dispatch({ type: SET_QUIZ_INTO_STATE, payload: null })
-    // First, dispatch an action to reset the quiz state (so the "Loading next quiz..." message can display)
-    // On successful GET:
-    // - Dispatch an action to send the obtained quiz to its state
-    fetch('http://localhost:9000/api/quiz/next')
-      .then(response => response.json())
-      .then(quiz => {
-        dispatch({ type: SET_QUIZ_INTO_STATE, payload: quiz})
-      })
-      .catch(error => {
-        console.error('Error fetching quiz:', error)
-      })
-  }
+
 }
-export function postAnswer(quizId, answerId) {
-  console.log('i am being called upon')
-  return function (dispatch) {
+export function postAnswer() {
+ 
     // On successful POST:
     // - Dispatch an action to reset the selected answer state
     // - Dispatch an action to set the server message to state
     // - Dispatch the fetching of the next quiz
-    fetch('http://localhost:9000/api/quiz/answer', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        quiz_id: quizId,
-        answer_id: answerId,
-      })
-    })
-    .then((response) => response.json())
-    .then((data) => {
-      dispatch(setInfoMessage(data.message))
-
-      dispatch(setSelectedAnswer(null))
-
-      dispatch(fetchQuiz())
-    })
-    .catch((error) => {
-      console.error('Error posting answer:', error)
-    })
-  }
+ 
 }
 export function postQuiz() {
   return function (dispatch) {
