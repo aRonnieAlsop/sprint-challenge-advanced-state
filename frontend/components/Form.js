048c7e2ee7f935d4ff966473d72693d262e80009
Arron Alsop <alsop.arron@gmail.com>
2023-10-23 21:42:19 -0700

    'needs a clean-up'

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,21 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import * as actionCreators from '../state/action-creators';
 
 export function Form(props) {
-  const [formData, setFormData] = useState({
+  const initialFormState = JSON.parse(localStorage.getItem('formData')) || {
     newQuestion: '',
     newTrueAnswer: '',
     newFalseAnswer: '',
-  });
+  }
+
+  const [formData, setFormData] = useState(initialFormState)
+
+  useEffect(() => {
+    localStorage.setItem('formData', JSON.stringify(formData))
+  }, [formData])
 
   const onChange = (evt) => {
     const { id, value } = evt.target;
     setFormData((prevData) => ({
       ...prevData,
       [id]: value,
-    }));
-  };
+    }))
+  }
 
   const onSubmit = (evt) => {
     evt.preventDefault()
@@ -27,6 +33,12 @@ export function Form(props) {
     props.postQuiz(payload)
       .then((quiz) => {
         props.setInfoMessage(`Congrats: "${quiz.question_text}" is a great question!`)
+        localStorage.removeItem('formData')
+        setFormData({
+          newQuestion: '',
+          newTrueAnswer: '',
+          newFalseAnswer: '',
+        })
       })
       .catch((error) => {
         console.log('Error Setting New Quiz:', error.message)
@@ -37,12 +49,27 @@ export function Form(props) {
   return (
     <form id="form" onSubmit={onSubmit}>
       <h2>Create New Quiz</h2>
-      <input maxLength={50} onChange={onChange} id="newQuestion" placeholder="Enter question" />
-      <input maxLength={50} onChange={onChange} id="newTrueAnswer" placeholder="Enter true answer" />
-      <input maxLength={50} onChange={onChange} id="newFalseAnswer" placeholder="Enter false answer" />
+      <input maxLength={50} 
+      onChange={onChange} 
+      id="newQuestion" 
+      placeholder="Enter question" 
+      value={formData.newQuestion}
+      />
+      <input maxLength={50} 
+      onChange={onChange} 
+      id="newTrueAnswer" 
+      placeholder="Enter true answer" 
+      value={formData.newTrueAnswer}
+      />
+      <input maxLength={50} 
+      onChange={onChange} 
+      id="newFalseAnswer" 
+      placeholder="Enter false answer" 
+      value={formData.newFalseAnswer}
+      />
       <button type="submit" id="submitNewQuizBtn">Submit new quiz</button>
     </form>
-  );
+  )
 }
 
 export default connect(null, actionCreators)(Form);
